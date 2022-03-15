c7c10debab0915836898b7cbddcaf22b591be2f6
unknown <aurimas0pupsys@gmail.com>
2022-03-15 22:52:06 +0200

    Commiting before doing refactor in case something goes wrong with db

--- PirmasDublis/src/main/java/persistence/EmployeesDAO.java
+++ PirmasDublis/src/main/java/persistence/EmployeesDAO.java
@@ -1,4 +1,23 @@
 package persistence;
 
+import entities.Employee;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@ApplicationScoped
 public class EmployeesDAO {
+
+    @Inject
+    private EntityManager em;
+
+    public void persist(Employee employee) { this.em.persist(employee); }
+
+    public List<Employee> findAll()
+    {
+        return em.createQuery("SELECT e FROM Employee as e", Employee.class)
+                .getResultList();
+    }
 }
